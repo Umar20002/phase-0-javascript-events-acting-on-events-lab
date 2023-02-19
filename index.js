772ee914a63f00dcae381f95be74dc48b07aee6e
Im Umar <iamumar@gm>
2023-02-19 13:35:25 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+//     const rightNumbers = dodger.style.right.replace("px", "")
+//     const right = parseInt{rightNumbers, 10};
+
+//     if (left < 0){
+//     dodger.style.leight = '${left + 1}px';
+// }
